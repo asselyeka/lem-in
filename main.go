78f8cda85b505a90f8dfa9381ff5046112edc9b6
assel <a.moldasheva@gmail.com>
2020-03-11 23:02:11 +0600

    fix: func Create Rooms

--- main.go
+++ main.go
@@ -1,109 +1,124 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+)
+
 type Room struct {
 	start              bool
 	end                bool
 	nameOfTheRoom      string
-	connectedWithRooms []*Room
+	connectedWithRooms []string
 	ants               bool
 	x, y               int
 }
 
-func CreateRooms (fileName string) []Room, int {
-		file, err := os.Open(fileName)
-		if err != nil {
-			fmt.Println(err.Error())
-			os.Exit(0)
+func CreateRooms(fileName string) ([]Room, int) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(0)
+	}
+	defer file.Close()
+
+	rawBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		panic(err)
+	}
+
+	lines := strings.Split(string(rawBytes), "\n")
+	var rooms []Room
+	var ants int
+	startRoom := 0
+	endRoom := 0
+
+	for indx, l := range lines {
+
+		if strings.Contains(l, "##start") == true && indx+1 < len(lines) {
+			startRoom = indx + 1
+			var tmpl Room
+			roomData := strings.Split(lines[indx+1], " ")
+			tmpl.start = true
+			tmpl.end = false
+			tmpl.nameOfTheRoom = roomData[0]
+			coorX, err1 := strconv.Atoi(roomData[1])
+			coorY, err2 := strconv.Atoi(roomData[2])
+			if err1 != nil || err2 != nil {
+				fmt.Println("ERROR")
+				os.Exit(0)
+			}
+			tmpl.x = coorX
+			tmpl.y = coorY
+			tmpl.connectedWithRooms = []string{}
+			rooms = append(rooms, tmpl)
+			continue
 		}
-		defer file.Close()
-
-		rawBytes, err := ioutil.ReadAll(file)
-		if err != nil {
-			panic(err)
+		if strings.Contains(l, "##end") == true && indx+1 < len(lines) {
+			endRoom = indx + 1
+			var tmpl Room
+			roomData := strings.Split(lines[indx+1], " ")
+			tmpl.start = false
+			tmpl.end = true
+			tmpl.nameOfTheRoom = roomData[0]
+			coorX, err1 := strconv.Atoi(roomData[1])
+			coorY, err2 := strconv.Atoi(roomData[2])
+			if err1 != nil || err2 != nil {
+				fmt.Println("ERROR")
+				os.Exit(0)
+			}
+			tmpl.x = coorX
+			tmpl.y = coorY
+			tmpl.connectedWithRooms = []string{}
+			rooms = append(rooms, tmpl)
+			continue
 		}
-
-		lines := strings.Split(string(rawBytes), "\n")
-		var rooms []Room
-		var ants int
-
-		for indx, l := range lines {
-			startRoom := 0
-			endRoom := 0
-			if strings.Contains(l,"##start") == true && indx+1 < len(lines){
-				startRoom = indx+1
-				var tmpl Room
-				roomData := strings.Split(lines[indx+1]," ")
-				tmpl.start = true
-				tmpl.end = false
-				tmpl.nameOfTheRoom = roomData[0]
-				tmpl.x, err1 := strconv.Atoi(roomData[1])
-				tmpl.y, err2 := strconv.Atoi(roomData[2])
-				if err1 != nil || err2 != nil {
-					fmt.Println("ERROR")
-					os.Exit(0)
-				}
-				rooms = append(rooms, tmpl)
+		if indx != 0 && (indx == startRoom || indx == endRoom) {
+			continue
+		}
+		roomData := strings.Split(l, " ")
+		if len(roomData) == 3 {
+			var tmpl Room
+			tmpl.start = false
+			tmpl.end = false
+			tmpl.nameOfTheRoom = roomData[0]
+			coorX, err1 := strconv.Atoi(roomData[1])
+			coorY, err2 := strconv.Atoi(roomData[2])
+			if err1 != nil || err2 != nil {
 				continue
 			}
-			if strings.Contains(l,"##end") == true && indx+1 < len(lines){
-				endRoom = indx+1
-				var tmpl Room
-				roomData := strings.Split(lines[indx+1]," ")
-				tmpl.start = false
-				tmpl.end = true
-				tmpl.nameOfTheRoom = roomData[0]
-				tmpl.x, err1 := strconv.Atoi(roomData[1])
-				tmpl.y, err2 := strconv.Atoi(roomData[2])
-				if err1 != nil || err2 != nil {
-					fmt.Println("ERROR")
-					os.Exit(0)
+			tmpl.x = coorX
+			tmpl.y = coorY
+			tmpl.connectedWithRooms = []string{}
+			rooms = append(rooms, tmpl)
+			continue
+		}
+		roomConnect := strings.Split(l, "-")
+		if len(roomConnect) == 2 {
+			room1 := roomConnect[0]
+			room2 := roomConnect[1]
+			fmt.Println(room1)
+			fmt.Println(room2)
+			for i, r := range rooms {
+				if room1 == r.nameOfTheRoom {
+					rooms[i].connectedWithRooms = append(rooms[i].connectedWithRooms, room2)
 				}
-				rooms = append(rooms, tmpl)
-				continue
-			}
-			if indx == startRoom || indx == endRoom {
-				continue
-			}
-			roomData := strings.Split(l, " ")
-			if len(roomData) == 3 {
-				var tmpl Room
-				tmpl.start = false
-				tmpl.end = false
-				tmpl.nameOfTheRoom = roomData[0]
-				tmpl.x, err1 := strconv.Atoi(roomData[1])
-				tmpl.y, err2 := strconv.Atoi(roomData[2])
-				if err1 != nil || err2 != nil {
-					continue
-				}
-				rooms = append(rooms, tmpl)
-			} else {
-				roomData = strings.Split(l, "-")
-				if len(roomData) == 2 {
-					room1 := roomData[0]
-					room2 := roomData[1]
-					var room1Link *Room
-					var room2Link *Room
-					for i, r := range rooms {
-						if room1 == r.nameOfTheRoom {
-							room1Link = *r
-						}
-						if room2 == r.nameOfTheRoom {
-							room2Link = *r
-						}
-					}
-					
-				} else {
-					ants, err := strconv.Atoi(l)
-					if err != nil {
-						continue
-					}
-
+				if room2 == r.nameOfTheRoom {
+					rooms[i].connectedWithRooms = append(rooms[i].connectedWithRooms, room1)
 				}
 			}
-
+			continue
 		}
 
-
+		ant, err := strconv.Atoi(l)
+		if err == nil {
+			ants = ant
+		}
+	}
+	return rooms, ants
 }
 
 func main() {
@@ -115,6 +130,24 @@ func main() {
 	fileName := args[1]
 	rooms, ants := CreateRooms(fileName)
 
+	fmt.Println(ants)
+	for _, r := range rooms {
+		fmt.Print("Name:")
+		fmt.Println(r.nameOfTheRoom)
+		fmt.Print("CoordX:")
+		fmt.Println(r.x)
+		fmt.Print("CoordY:")
+		fmt.Println(r.y)
+		fmt.Print("start:")
+		fmt.Println(r.start)
+		fmt.Print("end:")
+		fmt.Println(r.end)
+		fmt.Print("Connected with:")
+		for _, roo := range r.connectedWithRooms {
 
+			fmt.Print(roo, ", ")
+		}
+		fmt.Println()
+	}
 
 }
